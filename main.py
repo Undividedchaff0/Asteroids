d4191b7201ae9091e293d25798c09978baf46189
Undividedchaff0 <johannes626@outlook.com>
2026-09-14 13:29:29 +0200

    add player and update main game loop

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import pygame
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
+
+
 def main():
-    #print(f"Starting Asteroids with pygame version: {pygame.__version__}")
-    #print("Screen width: 1280")
-    #print("Screen height: 720")
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0.0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
 
     while True:
         log_state()
@@ -14,8 +19,11 @@ def main():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 return
+            dt += clock.tick(60) / 1000
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+
 
 if __name__ == "__main__":
     main()
