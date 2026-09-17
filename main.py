d275c347b3a25fa00620a817509b945918976803
Undividedchaff0 <johannes626@outlook.com>
2026-09-17 14:19:24 +0200

    fix bugs and clean up

--- main.py
+++ main.py
@@ -1,47 +1,60 @@
-
+from cmath import e
+import sys
 
 import pygame
 
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
-from logger import log_state
+from logger import log_event, log_state
 from player import Player
 
 
 def main():
 
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0.0
+
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+
     Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     Asteroid.containers = (asteroids, updatable, drawable)
-    AsteroidField.containers = (updatable)
-    asteroidfield = AsteroidField()
-    updatable.add(player, asteroids, asteroidfield)
-    drawable.add(player)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
 
-
-
-
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         log_state()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 return
-        dt = clock.tick(60) / 1000
+
+        for asteroid in asteroids:
+            if player.collides_with(asteroid):
+                log_event("player_hit")
+                print("Game Over!")
+                sys.exit()
+
+        updatable.update(dt)
+
         screen.fill("black")
-        for sprite in drawable:
-            sprite.draw(screen)
-        for sprite in updatable:
-            sprite.update(dt)
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
